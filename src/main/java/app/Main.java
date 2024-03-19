bdc23b3633c1e853b0656f6fbd7f2debddcda6c7
Magnewei <magnewei@icloud.com>
2024-03-19 19:07:53 +0100

    clean up

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -20,14 +20,12 @@ public class Main
     public static void main(String[] args)
     {
         // Initializing Javalin and Jetty webserver
-
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
         // Routing
-
         app.get("/", ctx -> ctx.render("index.html"));
         UserController.addRoutes(app, connectionPool);
         TimeZonesController.addRoutes(app, connectionPool);
