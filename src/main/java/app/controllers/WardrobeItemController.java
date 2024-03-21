7a45fcd857dd22622aa53f663a403ce7eb1abff0
moldamolda <danielherlev@hotmail.com>
2024-03-21 15:09:25 +0100

    diverse

--- src/main/java/app/controllers/WardrobeItemController.java
+++ src/main/java/app/controllers/WardrobeItemController.java
@@ -1,26 +1,33 @@
 package app.controllers;
 
+import app.entities.Task;
 import app.entities.User;
+import app.entities.WardrobeCategory;
 import app.entities.WardrobeItem;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.WardrobeCategoryMapper;
 import app.persistence.WardrobeItemMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.util.List;
+import java.util.Map;
 
 public class WardrobeItemController {
 
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/wardrober", ctx -> viewWardrobe(ctx, connectionPool));
         app.post("/deleteitem", ctx -> deleteItem(ctx, connectionPool));
         app.post("/edititem", ctx -> editItem(ctx, connectionPool));
+        //app.get("/wardroberedit", ctx -> WardrobeCategoryController.viewCategory(ctx,connectionPool));
     }
 
     private static void viewWardrobe(Context ctx, ConnectionPool connectionPool) {
         User user = ctx.sessionAttribute("currentUser");
         List<WardrobeItem> wardrobeItemList = WardrobeItemMapper.getAllItemsPerUser(user.getUserId(), connectionPool);
+        Map<Integer, WardrobeCategory> wardrobeCategoryMap = WardrobeCategoryMapper.getAllCategories(connectionPool);
+        ctx.sessionAttribute("categoryMap", wardrobeCategoryMap);
         ctx.attribute("itemList", wardrobeItemList);
         ctx.render("wardrober/index.html");
     }
@@ -31,14 +38,13 @@ public class WardrobeItemController {
 
         try {
             int itemId = Integer.parseInt(ctx.formParam("itemId"));
-            WardrobeItemMapper.delete(itemId,connectionPool);
-            List<WardrobeItem> itemList = WardrobeItemMapper.getAllItemsPerUser(user.getUserId(),connectionPool);
-            ctx.sessionAttribute("itemList",itemList);
-            // TODO: Change the above to maybe just attribute
+            WardrobeItemMapper.delete(itemId, connectionPool);
+            List<WardrobeItem> itemList = WardrobeItemMapper.getAllItemsPerUser(user.getUserId(), connectionPool);
+            ctx.sessionAttribute("itemList", itemList);
             ctx.render("wardrober/index.html");
 
 
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
 
             ctx.attribute("message", e.getMessage());
             ctx.render("wardrober/index.html");
@@ -46,33 +52,41 @@ public class WardrobeItemController {
         }
     }
 
-    public static void editItem(Context ctx, ConnectionPool connectionPool ){
+    public static void editItem(Context ctx, ConnectionPool connectionPool) {
 
         User user = ctx.sessionAttribute("currentUser");
+        List<WardrobeItem> itemList = ctx.sessionAttribute("itemList");
 
         try {
             //trækker ud af web-formularen
-            String brand = ctx.formParam("brand");
-            String description = ctx.formParam("description");
-            String color = ctx.formParam("color");
-            String size = ctx.formParam("size");
-            int price = Integer.parseInt(ctx.formParam("price"));
-            int categoryId = Integer.parseInt(ctx.formParam("category"));
+            int itemId = Integer.parseInt(ctx.formParam("itemId"));
+            WardrobeItem currentItem = null;
+            for (WardrobeItem i : itemList) {
+                if (i.getItemId() == itemId) {
+                    currentItem = i;
+                }
+            }
+            String brand = currentItem.getBrand();
+            String description = currentItem.getDescription();
+            String color = currentItem.getColor();
+            String size = currentItem.getSize();
+            int price = currentItem.getPrice();
+            int categoryId = currentItem.getCategoryId();
 
-            WardrobeItemMapper.createItem(brand,description,price,categoryId,color,size,connectionPool);
+            ctx.sessionAttribute("item", currentItem);
 
-            List<WardrobeItem> itemList = WardrobeItemMapper.getAllItemsPerUser(user.getUserId(),connectionPool);
 
-            ctx.attribute("itemList",itemList);
+
             //rendering: fletter html sammen med data, fortolker data
-            ctx.render("wardrober/index.html");
-        } catch (NumberFormatException e){
+            ctx.render("wardrober/edititem.html");
+
+        } catch (NumberFormatException e) {
 
             ctx.attribute("message", e.getMessage());
+            System.out.println(e.getMessage());
             ctx.render("wardrober/index.html");
 
+
         }
-
-
     }
 }
--- src/main/java/app/persistence/WardrobeItemMapper.java
+++ src/main/java/app/persistence/WardrobeItemMapper.java
@@ -1,6 +1,8 @@
 package app.persistence;
 
+import app.entities.Task;
 import app.entities.WardrobeItem;
+import app.exceptions.DatabaseException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WardrobeItemMapper {
-    public static List<WardrobeItem> getAllItemsPerUser(int userId, ConnectionPool connectionPool){
+    public static List<WardrobeItem> getAllItemsPerUser(int userId, ConnectionPool connectionPool) {
 
         List<WardrobeItem> itemList = new ArrayList<>();
 
@@ -22,7 +24,7 @@ public class WardrobeItemMapper {
         ) {
             ps.setInt(1, userId);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 int itemId = rs.getInt("item_id");
                 String brand = rs.getString("brand");
                 String color = rs.getString("color");
@@ -32,7 +34,7 @@ public class WardrobeItemMapper {
                 int categoryId = rs.getInt("category_id");
                 int id = rs.getInt("users_id");
 
-                WardrobeItem item = new WardrobeItem(itemId,brand,color,price,size,description,categoryId,id);
+                WardrobeItem item = new WardrobeItem(itemId, brand, color, price, size, description, categoryId, id);
                 itemList.add(item);
             }
 
@@ -41,7 +43,8 @@ public class WardrobeItemMapper {
         }
         return itemList;
     }
-    public static void createItem(String brand, String description, int price, int categoryId, String color, String size,  ConnectionPool connectionPool){
+
+    public static void createItem(String brand, String description, int price, int categoryId, String color, String size, ConnectionPool connectionPool) {
 
         WardrobeItem newItem = null;
 
@@ -52,12 +55,12 @@ public class WardrobeItemMapper {
                 PreparedStatement ps = connection.prepareStatement(sql);
         ) {
 
-            ps.setString(1,brand);
-            ps.setString(2,description);
-            ps.setInt(3,price);
-            ps.setInt(4,categoryId);
-            ps.setString(5,color);
-            ps.setString(6,size);
+            ps.setString(1, brand);
+            ps.setString(2, description);
+            ps.setInt(3, price);
+            ps.setInt(4, categoryId);
+            ps.setString(5, color);
+            ps.setString(6, size);
             ps.executeUpdate();
 
 
@@ -66,19 +69,50 @@ public class WardrobeItemMapper {
         }
     }
 
-    public static void delete(int itemId, ConnectionPool connectionPool){
+    public static void delete(int itemId, ConnectionPool connectionPool) {
 
         String sql = "DELETE FROM wardrobe_item WHERE item_id = (?)";
 
-        try(
+        try (
                 Connection connection = connectionPool.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql);
         ) {
-            ps.setInt(1,itemId);
+            ps.setInt(1, itemId);
             ps.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public static WardrobeItem getItemById(int itemId, ConnectionPool connectionPool) throws DatabaseException {
+        WardrobeItem item = null;
+
+        String sql = "select * from wardrobe_item where item_id = ?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, itemId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("item_id");
+                String brand = rs.getString("brand");
+                String color = rs.getString("color");
+                int price = rs.getInt("price");
+                String size = rs.getString("size");
+                String description = rs.getString("description");
+                int categoryId = rs.getInt("category_id");
+                int userId = rs.getInt("users_id");
+
+                item = new WardrobeItem(itemId, brand, color, price, size, description, categoryId, id);
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl ved hentning af task med id" + item.getItemId());
+
+
+        }
+        return item;
     }
 }
+
